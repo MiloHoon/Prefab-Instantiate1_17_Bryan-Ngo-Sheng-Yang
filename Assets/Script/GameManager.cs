9acef919e14aaf8455e30c273058620c44294b21
MiloHoon <brainlessx3@gmail.com>
2021-01-20 14:13:59 +0800

    Updated

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -13,7 +13,10 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gmInstance == null)
+        {
+            gmInstance = this;
+        }
     }
 
     // Update is called once per frame
